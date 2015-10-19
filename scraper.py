cdc700bcdf1dd3391f27ef840f0365c14d7d7eb8
AlphaZuloZulo <yunli2012@gmail.com>
2015-10-18 22:03:57 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,8 +10,8 @@ def duckint(i):
         return i
 
 # Blank Python
-lookup = ['Name','Team','GP','GS','MIN','W','L','OTL','EGA','GA','GAA','SA','SV','SVP','SO']
-num =    [0,1,2,4,6,8,9,11,13,15,17,19,21,23,25]
+lookup = ['Name','Team','GP','GS','MIN','L','OTL','EGA','GA','GAA','SA','SV','SVP','SO']
+num =    [0,1,2,4,6,9,11,13,15,17,19,21,23,25]
 
 lstring = ', '.join(lookup)
 
@@ -19,8 +19,8 @@ scraperwiki.sqlite.execute('create table if not exists score (%s)'%lstring)
 
 #url='http://sports.yahoo.com/nhl/stats/byposition?pos=D'
 #url='http://sports.yahoo.com/nhl/stats/byposition?pos=D&conference=NHL&year=season_2015&qualified=1'
-#url='http://sports.yahoo.com/nhl/stats/byposition?pos=G&conference=NHL&year=season_2015&qualified=1'
-url='http://sports.yahoo.com/nhl/stats/byposition?pos=G&conference=NHL&year=season_2015&qualified=1&sort=LAST_NAME'
+url='http://sports.yahoo.com/nhl/stats/byposition?pos=G&conference=NHL&year=season_2015&qualified=1'
+#url='http://sports.yahoo.com/nhl/stats/byposition?pos=G&conference=NHL&year=season_2015&qualified=1&sort=LAST_NAME'
 
 html=requests.get(url).content
 root=lxml.html.fromstring(html)
@@ -32,7 +32,7 @@ for row in rows:
     cells=[cell.text_content().strip() for cell in row.xpath('td[@class="yspscores"]')]
     for i,n in enumerate(num):
         data[lookup[i]]=duckint(cells[n])
-    data['Pts']=duckint(row.xpath('descendant-or-self::span[@class="yspscores"]')[0].text_content().strip())
+    data['W']=duckint(row.xpath('descendant-or-self::span[@class="yspscores"]')[0].text_content().strip())
     builder.append(data)
     
 scraperwiki.sqlite.save(table_name='score', data=builder, unique_keys=['Name'])
