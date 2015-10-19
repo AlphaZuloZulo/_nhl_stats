89bba785b9b95885281c058205f5e541daba95a1
AlphaZuloZulo <yunli2012@gmail.com>
2015-10-18 21:33:47 -0400

    percent

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ def duckint(i):
         return i
 
 # Blank Python
-lookup = ['Name','Team','GP','GS','MIN','W','L','OTL','EGA','GA','GAA','SA','SV','SV%','SO']
+lookup = ['Name','Team','GP','GS','MIN','W','L','OTL','EGA','GA','GAA','SA','SV','SVP','SO']
 num =    [0,1,2,4,6,8,10,12,14,16,18,20,22,24,26]
 
 lstring = ', '.join(lookup)
